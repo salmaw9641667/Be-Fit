f11607eb74d72d1c464a667ff856013192247ea2
salma <salma.W9641667@gmail.com>
2023-10-19 04:34:42 -0700

    starting project with login activity

---
+++ app/src/main/java/uk/ac/tees/mad/W9641667/login.kt
@@ -0,0 +1,14 @@
+package uk.ac.tees.mad.W9641667
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
