3eba10a1dfcc8ac0c41319fbb579487d0ace0397
salmaw9641667 <148349080+salmaw9641667@users.noreply.github.com>
2023-12-11 23:29:01 +0000

    Add files via upload

---
+++ app/src/main/java/uk/ac/tees/mad/W9641667/data/Exercise.kt
@@ -0,0 +1,10 @@
+package uk.ac.tees.mad.W9641667.data
+
+data class Exercise(
+    val name: String,
+    val type: String,
+    val muscle: String,
+    val equipment: String,
+    val difficulty: String,
+    val instructions: String
+)
---
+++ app/src/main/java/uk/ac/tees/mad/W9641667/data/Rec.kt
@@ -0,0 +1,12 @@
+package uk.ac.tees.mad.W9641667.data
+
+data class Rec(
+    val calories: String,
+    val carbs: String,
+    val fat: String,
+    val id: Int,
+    val image: String,
+    val imageType: String,
+    val protein: String,
+    val title: String
+)
